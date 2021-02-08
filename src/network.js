643df070a0c7b0a558354e208767aa75805afef3
AiBlocks <dev@aiblocks.io>
2021-02-08 19:25:39 +0300

    aiblocks js base

---
+++ src/network.js
@@ -0,0 +1,10 @@
+/**
+ * Contains passphrases for common networks:
+ * * `Networks.PUBLIC`: `Public Global AiBlocks Network ; December 2020`
+ * * `Networks.TESTNET`: `Test AiBlocks Network ; December 2020`
+ * @type {{PUBLIC: string, TESTNET: string}}
+ */
+export const Networks = {
+  PUBLIC: 'Public Global AiBlocks Network ; December 2020',
+  TESTNET: 'Test AiBlocks Network ; December 2020'
+};
